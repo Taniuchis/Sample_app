cfb0401d2167661456ebdacd3aa72d627bd3e558
Tania Munoz <tania.m.jimenez24@gmail.com>
2018-10-03 11:56:54 -0500

    gem devise

--- Gemfile
+++ Gemfile
@@ -32,7 +32,9 @@ gem 'jbuilder', '~> 2.5'
 
 # Use Capistrano for deployment
 # gem 'capistrano-rails', group: :development
-
+gem 'devise'
+gem 'bootstrap-sass'
+gem 'font-awesome-sass', '~> 5.2'
 # Reduces boot times through caching; required in config/boot.rb
 gem 'bootsnap', '>= 1.1.0', require: false
 
---
+++ app/models/user.rb
@@ -0,0 +1,11 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable,
+         :authentication_keys => [:username]
+  validates :email, uniqueness: true
+  validates :username, uniqueness: true
+
+end
